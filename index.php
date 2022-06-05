a8e2931eb80f1d5ded45eb711432515f5eb98470
Henry Carvalho <47277476+HenryCL@users.noreply.github.com>
2022-06-05 14:14:34 -0300

    Add files via upload
    Site using PHP, Css and Bootstrap

---
+++ index.php
@@ -0,0 +1,9 @@
+    <?php include_once("templates/header.php");?>
+
+<body>
+    <center>
+        <h1> Testando</h1>    
+    <i class="fab fa-accessible-icon" style="font-size:50px; color:red"></i>
+    </center>
+
+    <?php include_once("templates/header.php");?>
